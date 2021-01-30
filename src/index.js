84eda511361a7a6ff62eb14f6e0859ac7197fd56
Pavel.K <pavelweb74@gmail.com>
2021-01-30 20:44:02 +0500

    Add redux and create reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { compose, createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./redux/rootReducer";
+
 import App from "./App";
+
+const store = createStore(
+  rootReducer,
+  compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 
 render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
